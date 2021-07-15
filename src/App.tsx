73e4230d9c943fd8bcaf894ef9a51950c794886d
Geraldo Istalin <geraldoboueres@gmail.com>
2021-07-15 19:00:58 -0300

    CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import styled from 'styled-components'
+import { GlobalStyle } from './styles/global'
 
+const Title = styled.h1`
+font-size: 64px;
+color: #8257e6;
 
-function App() {
+`
+
+export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1> Hello World </h1>
+      <GlobalStyle />
     </div>
   );
 }
-
-export default App;
